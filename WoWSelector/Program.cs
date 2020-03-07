862fb73b4e476744014820aae2ee78481847f36d
James Webber <James.Webber@cansfordlabs.co.uk>
2020-03-07 21:44:16 +0000

    updated the grammar when informing the user

--- WoWSelector/Program.cs
+++ WoWSelector/Program.cs
@@ -9,12 +9,16 @@ namespace WoWSelector
         {
             while (true)
             {
+                // get the character to play
                 var controller = new SelectorController();
                 var randomCharacter = controller.GetRandomRace();
 
+                // tell the user
+                var grammar = IsVowel(randomCharacter.Name.ToString()[0]) ? "an" : "a";
                 Console.WriteLine($"For the {randomCharacter.Faction}!");
-                Console.WriteLine($"You will play a {randomCharacter.Name} {randomCharacter.Type}.");
+                Console.WriteLine($"You will play {grammar} {randomCharacter.Name} {randomCharacter.Type}.");
 
+                // see if they want to go again
                 Console.WriteLine("Try again? Y/N?");
                 var response = Console.ReadLine();
 
@@ -24,6 +28,18 @@ namespace WoWSelector
 
             Console.WriteLine("Hit any key to exit.");
             Console.ReadLine();
+        }
+
+        private static bool IsVowel(char character)
+        {
+            char[] vowels = { 'A', 'E', 'I', 'O', 'U' };
+            foreach (var vowel in vowels)
+            {
+                if (character == vowel)
+                    return true;
+            }
+
+            return false;
         }
     }
 }
